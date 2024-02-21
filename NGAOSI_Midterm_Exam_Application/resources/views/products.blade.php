80ee769eea851aa742e3cbbdbac5e3d5b49df024
BNGAOSI <https://github.com/BNGAOSI/>
2024-02-21 09:36:49 +0800

    Add controllers and blade view that contains list

---
+++ NGAOSI_Midterm_Exam_Application/resources/views/products.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config('app.namej')}}</title>
+</head>
+<body>
+    <div class="container-py">
+        <h1>List of Gadgets</h1>
+        <ul>
+            <li>Apple Vision Pro</li>
+            <li>Iphone 15</li>
+            <li>Nintendo Switch</li>
+            <li>Apple Watch</li>
+            <li>Oculus Rift Vr</li>
+        </ul>
+    </div>
+</body>
+</html>
